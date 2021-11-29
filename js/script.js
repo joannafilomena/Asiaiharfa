52567c667a54fc2935a7a232983cde221f5e29f4
Joanna Kamińska <joannakaminska-92@wp.pl>
2021-11-30 00:17:58 +0100

    Animations added, new sections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            document.querySelectorAll(".animated")[0].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[1].classList.add("fadeInRight");
+        }
+    })
+})
+
+observer.observe(document.querySelector(".first"));
+
+const observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            document.querySelectorAll(".animated")[2].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[3].classList.add("fadeInRight");
+        }
+    })
+})
+
+observer2.observe(document.querySelector(".second"));
+
+const observer3 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            document.querySelectorAll(".animated")[4].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[5].classList.add("fadeInRight");
+        }
+    })
+})
+
+observer3.observe(document.querySelector(".third"));
+
+const observer4 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            document.querySelectorAll(".animated")[6].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[7].classList.add("fadeInRight");
+        }
+    })
+})
+
+observer4.observe(document.querySelector(".fourth"));
 
 
 /*let buttonChange = document.querySelector(".change");
@@ -11,7 +54,7 @@ buttonChange.addEventListener("click", () => {
     if (paragraph.classList.contains("pl")) {
         paragraph.innerText = "This is english";
         buttonChange.innerText = "Change language";
-    } else { 
+    } else {
         paragraph.innerText = "To jest polski";
         buttonChange.innerText = "Zmień język" }
 });*/
